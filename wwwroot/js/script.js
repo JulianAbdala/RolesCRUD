ffeaf3b08fc580d55fcfe95a66c3252f4fdba799
Julian Abdala <j.abdala@kellerhoff.com.ar>
2023-08-28 13:56:08 -0300

    Added GoBack button and formating

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,62 +1,67 @@
 //MODIFICAR ROL
   
-  $(document).ready(function() {
-    $('#modificarUsuarioBtn').click(function() {
-        const idrol = $('#idrol').val();
-        const newName = $('#newName').val();
-        
-        modificarUsuario(idrol, newName);
-    });
+$(document).ready(function() {
+  $('#modificarUsuarioBtn').click(function() {
+      const idrol = $('#idrol').val();
+      const newName = $('#newName').val();
+      
+      modificarUsuario(idrol, newName);
+  });
 });
 
 //BUSCAR ROL
 
 $(document).ready(function () {
-  $('#buscarForm').submit(function (event) {
-      event.preventDefault();
+$('#buscarForm').submit(function (event) {
+    event.preventDefault();
 
-      var nombre = $('#buscarrol').val(); 
+    var nombre = $('#buscarrol').val(); 
 
-      $.ajax({
-          url: '/welcome/buscarrol/' + nombre,
-          type: 'GET',
-          success: function (data) {
-              $('#rolesContainer').html(data);
-          },
-          error: function (error) {
-              console.log(error);
-          }
-      });
-  });
+    $.ajax({
+        url: '/welcome/buscarrol/' + nombre,
+        type: 'GET',
+        success: function (data) {
+            $('#rolesContainer').html(data);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+});
 });
 
 //CREAR ROL
 
 $(document).ready(function() {
-  $('#crearRolBtn').click(function() {
-      const rolNombre = $('#rolName').val();
-      
-      CrearRol(rolNombre);
-  });
+$('#crearRolBtn').click(function() {
+    const rolNombre = $('#rolName').val();
+    
+    CrearRol(rolNombre);
+});
 });
 
 // CREACION GRILLA
 
 $(document).ready(function () {
-  var rolesContainer = $("#rolesContainer");
+var rolesContainer = $("#rolesContainer");
 
-  var table = $("<table>");
-  var headerRow = $("<tr>");
-  headerRow.append($("<th>").text("Código de Rol"));
-  headerRow.append($("<th>").text("Nombre de Rol"));
-  table.append(headerRow);
+var table = $("<table>");
+var headerRow = $("<tr>");
+headerRow.append($("<th>").text("Código de Rol"));
+headerRow.append($("<th>").text("Nombre de Rol"));
+table.append(headerRow);
 
-  roles.forEach(function (rol) {
-    var rolRow = $("<tr>");
-    rolRow.append($("<td>").text(rol.rol_codRol));
-    rolRow.append($("<td>").text(rol.rol_Nombre));
-    table.append(rolRow);
+roles.forEach(function (rol) {
+  var rolRow = $("<tr>");
+  rolRow.append($("<td>").text(rol.rol_codRol));
+  rolRow.append($("<td>").text(rol.rol_Nombre));
+  table.append(rolRow);
 });
 
-  rolesContainer.append(table);
+rolesContainer.append(table);
+});
+
+//Boton ATRÁS
+document.getElementById("volverAtras").addEventListener("click", function() {
+window.location.href = "welcome.cshtml";
 });
